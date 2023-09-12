f94d9e3e1b52c447b1430e267f8a182902fe7588
Arun <arunsrinivasan333@gmail.com>
2023-09-12 15:34:17 +0530

    hi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program{
+static void Main()
+{
+LinkedList <string> list = new LinkedList<string>();
+list.AddLast("Ruban");
+    list.AddLast("Sanjai");
+    list.AddLast("Asha");
+    var NodeGo = list.AddLast("Karnisha");
+var friend= list.AddFirst("Arun");
+list.AddBefore(friend,"Sindhu");
+    Console.WriteLine("Initial list");
+    foreach (string i in list)
+    {
+        Console.WriteLine(i);
+    }
+
+    Console.WriteLine("Array afer updation");
+    list.AddAfter(NodeGo,"Kathir");
+
+    foreach(string i in list){
+        Console.WriteLine(i);
+    }
+
+
+LinkedList <int> list1 = new LinkedList <int>();
+list1.AddLast(1);
+foreach(int i in list1){
+Console.WriteLine(i);}
+}
+}
